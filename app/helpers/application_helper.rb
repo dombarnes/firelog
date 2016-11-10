5a868f889d126cce10e95ad08c73a9c81fb540ea
Dom Barnes <dom@dombarnes.com>
2016-11-10 17:12:31 +0000

    Fixing static assets hopefully

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelpers
+  def current?(path = '/')
+    request.path_info == path ? 'current' : nil
+  end
+
+  def link_to(text, url, opts = {})
+    attributes = ''
+    opts.each do |k, v|
+      attributes << k.to_s << '="' << v << '" ' if k && v
+    end
+    %(<a href="#{url}" #{attributes}>#{text}</a>)
+  end
+end
--- config.ru
+++ config.ru
@@ -4,6 +4,7 @@ Dir['./app/{helpers,controllers,models}/*.rb'].each { |file| require file }
 
 # GZip compession
 use Rack::Deflater
+use Rack::Static, :urls => ['/stylesheets', '/javascripts'], :root => 'public'
 
 map('/events') { run EventController }
 map('/readings') { run ReadingController }
