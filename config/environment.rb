d4cb8da5a5900fd12e9ccad343142377f2e8af16
Dom Barnes <dom@dombarnes.com>
2016-11-23 11:27:16 +0000

    Switching to puma

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-require './app'
 Dir.glob('./app/{models,helpers,extensions,controllers}/*.rb').each {  |file| 
   require file 
 }
+require './app'
